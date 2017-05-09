acf150e2387f78ff16e61fbc92f20ed8962d97b0
Kristoffer Ström <kristoffer@rymdkoloni.se>
2017-05-09 11:43:15 +0200

    freezer docs

---
+++ rustdoc/freezer/doc/aodag/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Versioned","A value that can be updated, keeping old versions available but read-only"]],"type":[["Address","An address pointing at a value in the DAG"]]});
